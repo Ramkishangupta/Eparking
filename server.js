3831dc9dfa0e468acb0d18382e7f550912fd5126
GUJJAR84 <PREETCHECHI100@GMAIL.COM>
2025-01-19 22:37:38 +0530

    added Excel feature

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const slotRoutes = require('./routes/slots')
-
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
